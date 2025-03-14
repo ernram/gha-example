2c74b1f7487533d3de111f41bbc53c3f3d5ae93c
Ernest Ramirez <ernest@ernestramirez.tech>
2025-03-13 19:30:11 -0500

    pushing a code change to pr branch

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	for {
-		fmt.Println("This is my new version of my Go application!")
+		fmt.Println("This is my new version of my Go application (NEW)!")
 		time.Sleep(3 * time.Second)
 	}
 } 
